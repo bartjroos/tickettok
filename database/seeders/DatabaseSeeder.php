9de374cd3393f56242f6a85eff618527f75b6e1d
Bart Roos <bart@studiokaa.co>
2023-11-07 15:31:48 +0100

    US3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Event::factory(100)->create();
+
+        \App\Models\Role::create(['name' => 'admin']);
+        \App\Models\Role::create(['name' => 'klant']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::view('dashboard', 'dashboard')->name('admin.dashboard');
+    // Route::resource('events');
+});
 
 require __DIR__.'/auth.php';
